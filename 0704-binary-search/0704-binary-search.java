8ca90aceaa2b42c59e122c5ef2a42532ce001c84
Himanshu Dixit <himanshudixit09531@gmail.com>
2026-09-21 19:25:58 +0530

    Time: 0 ms (100%), Space: 48.2 MB (84.6%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] arr, int target) {
+    
+       
+        int low = 0;
+        int high = arr.length - 1;
+
+       
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] < target) 
+                low = mid + 1;
+            else if (arr[mid] > target) 
+                high = mid - 1;
+            else {
+               return mid;
+              
+            }
+        }
+
+        return -1;
+    }
+}
+
+    
